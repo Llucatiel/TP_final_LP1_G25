cb76a1fe5e9aa80e9b17627e2d89b025a4595b34
oxcar30 <oscarperdomo896@gmail.com>
2024-05-17 13:44:34 -0300

    7/05
    7/05

--- D&V/cJinete.cpp
+++ D&V/cJinete.cpp
@@ -1,1 +1,14 @@
 #include "cJinete.h"
+
+void cJinete::asignarDragon(cDragon* dragon)
+{
+	this->dragon = dragon;
+}
+
+void cJinete::entrenar()
+{
+	int buffeo;
+	notaEntrenar = rand() % 11;
+
+	buffeo = (notaEntrenar - 5) / 2;
+}
--- D&V/cJinete.h
+++ D&V/cJinete.h
@@ -7,13 +7,28 @@ enum eResultado {
 };
 typedef eResultado resultado;
 
+enum eRelacion {
+	pobre = 0, mala, mediana, buena, superbuena
+};
+typedef eRelacion relacion;
+
+
 class cJinete :public cVikingo
 {
 	string apodo;
 	string colordepelo;
 	cDragon* dragon;
+	int notaEntrenar = 5;
+	int buffeo = 0;
+	relacion relacionDragon = pobre;
 
-	
+public:
+	void asignarDragon(cDragon* dragon);
+	string getApodo() { return apodo; }
+	string setApodo(string apodito) {
+		this->apodo = apodito;
+	}
+	void entrenar();
 
 };
 
